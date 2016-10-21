d255c3897e4ebb25e67e84598c1c8dcff1941d3c
waterstonemf <waterstonemf@outlook.com>
2016-10-21 17:44:56 +0800

    add PE format introduction

---
+++ docs/PE File Format/Sample/ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace ConsoleApplication4
+{
+   [Flags]
+    enum NodeType
+    {
+        None = 0,
+        Single = -1,
+        Double = -2,
+        Three = -4
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NodeType t = NodeType.Single | NodeType.Three;
+
+            if (t == NodeType.None)
+            {
+                Console.WriteLine("Hello None");
+            }else
+            {
+                Console.WriteLine(t.ToString());
+            }
+
+            Console.Read();
+        }
+    }
+}
